c2d56764b63358391ed7b154b4449830940ffb35
Lug16 <lug@MacBook-de-Lug.local>
2016-08-16 00:02:15 -0500

    Changing render controls

--- js/sapientia.js
+++ js/sapientia.js
@@ -1,31 +1,59 @@
-(function(global){
+(function (global) {
     'use strict';
-
-    global.sapientia = function(url)
-    {
-        constructor(url);
-    }
-    
-    var objectBuilder = function(controlId)
-    {
-        var control = $(this);
-        var id = controlId || control.attr('id');
-
-        control.addClass('sap-jumbotron').text('test');
-        //console.info(url);
-    }
-    
-    function constructor(url)
-    {
-        var $=global.$;
-        var culture = {material:'material',content:'contenido',questions:'preguntas',answers:'respuestas'};
-        var version = '1.0.0';
-        var self = this;
-        var url = url;
+    var $ = global.$;
+    var version = '1.0.0';
+    var culture = {material:'material',content:'contenido',questions:'preguntas',answers:'respuestas'};
+    var controls = [];
         
-        var sapientia = objectBuilder;
+    function sapientia(url) {
+        var sapientia = objectSetter;
+
+        $.getJSON(url, loadObjects);
 
         $.prototype.sapientia = sapientia;
     }
+    
+    global.sapientia = function (url) {
+        sapientia(url);
+    }
+    
+    var objectSetter = function(controlId) {
+        var control = $(this);
+        var id = controlId || control.attr('id');
 
+        controls.push({'id':id, 'content':null});
+    }
+    
+    function loadObjects(data)
+    {   
+        $.each(controls,function(i,item){
+            item.content = objectGetter(data,item.id);     
+        });
+        
+        console.info(controls);
+    }
+    
+    function objectGetter(data, nodeName){
+        var result = null;
+
+        for(var t in data) {
+          if(data[t].constructor===Array)
+          {
+              if(t===nodeName)
+              {
+                  result = data[t];
+              }else{
+                  objectGetter(data[t])
+              }
+          }
+        }
+        
+        return result;
+    }
+    
+    function contentAssembler(contentNode)
+    {
+        
+    }
+    
 })(window);
