4b0c16a57cba07060fc3393cfd533cd9341cf756
Lug16 <lug@MacBook-de-Lug.local>
2016-08-27 23:38:34 -0500

    Materiales
    Agregando render de video

--- js/sapientia.js
+++ js/sapientia.js
@@ -2,58 +2,124 @@
     'use strict';
     var $ = global.$;
     var version = '1.0.0';
-    var culture = {material:'material',content:'contenido',questions:'preguntas',answers:'respuestas'};
-    var controls = [];
-        
+    var container = null;
+    var ad = false;
+    var db = null;
+    var labels ={pdf:'Pdf',pps:'Presentation',vid:'Video'}
+
+    //constructor recibiendo una url en busca del contenido
     function sapientia(url) {
         var sapientia = objectSetter;
-
-        $.getJSON(url, loadObjects);
-
+        
+        $.getJSON(url,  loadObjects);
         $.prototype.sapientia = sapientia;
     }
     
+    //instancia en el objeto global una instancia
     global.sapientia = function (url) {
         sapientia(url);
     }
     
-    var objectSetter = function(controlId) {
-        var control = $(this);
-        var id = controlId || control.attr('id');
-
-        controls.push({'id':id, 'content':null});
+    global.sapientia.labels = labels;
+    global.sapientia.version = version;
+    
+    //Identificador cuando se utiliza un selector
+    var objectSetter = function(autodraw) {
+        ad = autodraw;
+        container = $(this); 
     }
     
-    function loadObjects(data)
-    {   
-        $.each(controls,function(i,item){
-            item.content = objectGetter(data,item.id);     
-        });
-        
-        console.info(controls);
-    }
-    
-    function objectGetter(data, nodeName){
-        var result = null;
-
-        for(var t in data) {
-          if(data[t].constructor===Array)
-          {
-              if(t===nodeName)
-              {
-                  result = data[t];
-              }else{
-                  objectGetter(data[t])
-              }
-          }
-        }
-        
-        return result;
-    }
-    
-    function contentAssembler(contentNode)
+    //Cuando recibe los objetos de la consulta JSON
+    var loadObjects = function(data)
     {
-        
+        console.info(data);
+        db = data;
+        container.data("info",data);
+        build();
+    }
+    
+    //Construir los objetos
+    var build = function()
+    {
+        if(ad){
+            draw();
+        }
+    }
+    
+    var draw = function()
+    {
+        var material = db.content.material;
+        drawMaterial(material,ad);
+    }
+    
+    function applyClass(selector,oclass,withStyle)
+    {
+        if(withStyle){
+            selector.addClass(oclass);
+        }
+    }
+    
+    var drawMaterial = function(material,withStyle)
+    {
+        if(material){
+            var ul = $('<ul>');
+            ul.attr('id','materials');    
+            applyClass(ul,'nav nav-tabs nav-justified',withStyle);
+            
+            var expositor = $('<div>');
+            expositor.attr('id','expositor');
+            
+            $.each(material,function(i,obj){
+                var li = $('<li>');
+                var a = $('<a>');
+                
+                if(i===0){
+                    li.addClass('active');
+                }
+                
+                li.click(function(){
+                    $('#materials li').removeClass('active');
+                    $(this).addClass('active');
+                    var dataItem = $(this).data().info;
+                    var type = dataItem.type;
+                    expositor.empty();
+                    var content = null;
+                    
+                    switch(type){
+                        case 'vid':
+                            expositor.addClass('embed-responsive embed-responsive-16by9');
+                            
+                            content = $('<video>');
+                            content.attr('controls','controls');
+                            content.addClass('embed-responsive-item');
+                            var src = $('<source>');
+                            
+                            src.attr('src',dataItem.location);
+                            src.attr('type','video/' + dataItem.location.slice(-3));
+                            
+                            content.append(src);
+                            
+                            
+                            break;
+                    }
+                    
+                    expositor.append(content);
+
+                });
+                
+                li.data('info',obj);
+                li.attr('role','material')
+                a.attr('href','#');
+                a.text(labels[obj.type]);
+                
+                li.append(a);
+                ul.append(li);
+            })
+            
+            container.append(ul);
+            
+            container.append(expositor);
+        }
     }
     
 })(window);
