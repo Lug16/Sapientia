1d736053748ead6087c55e731ae90d93b855da38
Lug16 <lug@MacBook-de-Lug.local>
2016-08-29 00:23:20 -0500

    Working on ppts
    Finally a way to display them

--- js/sapientia.js
+++ js/sapientia.js
@@ -5,121 +5,153 @@
     var container = null;
     var ad = false;
     var db = null;
-    var labels ={pdf:'Pdf',pps:'Presentation',vid:'Video'}
+    var labels = {
+        pdf: 'Pdf',
+        pps: 'Presentation',
+        vid: 'Video'
+    }
 
     //constructor recibiendo una url en busca del contenido
     function sapientia(url) {
         var sapientia = objectSetter;
-        
-        $.getJSON(url,  loadObjects);
+
+        $.getJSON(url, loadObjects);
         $.prototype.sapientia = sapientia;
     }
-    
+
     //instancia en el objeto global una instancia
     global.sapientia = function (url) {
         sapientia(url);
     }
-    
+
     global.sapientia.labels = labels;
     global.sapientia.version = version;
-    
+
     //Identificador cuando se utiliza un selector
-    var objectSetter = function(autodraw) {
+    var objectSetter = function (autodraw) {
         ad = autodraw;
-        container = $(this); 
+        container = $(this);
     }
-    
+
     //Cuando recibe los objetos de la consulta JSON
-    var loadObjects = function(data)
-    {
+    var loadObjects = function (data) {
         console.info(data);
         db = data;
-        container.data("info",data);
+        container.data("info", data);
         build();
     }
-    
+
     //Construir los objetos
-    var build = function()
-    {
-        if(ad){
+    var build = function () {
+        if (ad) {
             draw();
         }
     }
-    
-    var draw = function()
-    {
+
+    var draw = function () {
         var material = db.content.material;
-        drawMaterial(material,ad);
+        drawMaterial(material, ad);
     }
-    
-    function applyClass(selector,oclass,withStyle)
-    {
-        if(withStyle){
+
+    function applyClass(selector, oclass, withStyle) {
+        if (withStyle) {
             selector.addClass(oclass);
         }
     }
-    
-    var drawMaterial = function(material,withStyle)
-    {
-        if(material){
+
+    function renderVideo(dataItem) {
+        var isFile = dataItem.location.slice(-4, -3) === '.';
+        var content = null;
+
+        if (isFile) {
+            var content = $('<video>');
+            content.attr('controls', 'controls');
+
+            var src = $('<source>');
+
+            src.attr('src', dataItem.location);
+            src.attr('type', 'video/' + dataItem.location.slice(-3));
+
+            content.append(src);
+        } else {
+            var content = $('<iframe>');
+
+            if (dataItem.location.search('youtu') > -1) { //Si es un video de youtube
+                if (dataItem.location.search('embed') > -1) {
+                    content.attr('src', dataItem.location)
+                } else {
+                    content.attr('src', dataItem.location.replace('youtube.com/', 'youtube.com/embed/').replace('watch?v=', '').replace('youtu.be/', 'www.youtube.com/embed/'))
+                }
+            }
+        }
+
+        content.addClass('embed-responsive-item');
+
+        return content;
+    }
+
+    var drawMaterial = function (material, withStyle) {
+        if (material) {
             var ul = $('<ul>');
-            ul.attr('id','materials');    
-            applyClass(ul,'nav nav-tabs nav-justified',withStyle);
-            
+            ul.attr('id', 'materials');
+            applyClass(ul, 'nav nav-tabs nav-justified', withStyle);
+
             var expositor = $('<div>');
-            expositor.attr('id','expositor');
-            
-            $.each(material,function(i,obj){
+            expositor.attr('id', 'expositor');
+
+            var currentTabs = [];
+
+            $.each(material, function (i, obj) {
                 var li = $('<li>');
                 var a = $('<a>');
-                
-                if(i===0){
+
+                if (i === 0) {
                     li.addClass('active');
                 }
-                
-                li.click(function(){
+
+                li.data('info', obj);
+                li.attr('role', 'material')
+                a.attr('href', '#');
+
+                var title = '';
+
+                var indexTab = currentTabs.lastIndexOf(labels[obj.type]);
+
+                if (indexTab >= 0) {
+                    a.text(labels[obj.type] + '-' + indexTab);
+                } else {
+                    a.text(labels[obj.type]);
+                }
+
+                currentTabs.push(labels[obj.type]);
+
+                li.append(a);
+                ul.append(li);
+
+                li.click(function () {
                     $('#materials li').removeClass('active');
                     $(this).addClass('active');
                     var dataItem = $(this).data().info;
                     var type = dataItem.type;
                     expositor.empty();
                     var content = null;
-                    
-                    switch(type){
-                        case 'vid':
-                            expositor.addClass('embed-responsive embed-responsive-16by9');
-                            
-                            content = $('<video>');
-                            content.attr('controls','controls');
-                            content.addClass('embed-responsive-item');
-                            var src = $('<source>');
-                            
-                            src.attr('src',dataItem.location);
-                            src.attr('type','video/' + dataItem.location.slice(-3));
-                            
-                            content.append(src);
-                            
-                            
-                            break;
+
+                    switch (type) {
+                    case 'vid':
+                        expositor.addClass('embed-responsive embed-responsive-16by9');
+                        content = renderVideo(dataItem);
+                        break;
                     }
-                    
+
                     expositor.append(content);
 
                 });
-                
-                li.data('info',obj);
-                li.attr('role','material')
-                a.attr('href','#');
-                a.text(labels[obj.type]);
-                
-                li.append(a);
-                ul.append(li);
             })
-            
+
             container.append(ul);
-            
+
             container.append(expositor);
         }
     }
-    
+
 })(window);
